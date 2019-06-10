b5e4ad48dd10aec5abbe2550d31a1149721d661f
suveng <suveng@163.com>
2019-06-10 18:06:24 +0800

    n2-增加注释

--- src/n2/Solution.java
+++ src/n2/Solution.java
@@ -1,11 +1,22 @@
 package n2;
 
-/**
- * description:
- * @author suwenguang
- * @date 2019/6/10
- * @version 1.0.0
- **/
+/*
+ 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+ 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+ 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ 示例：
+
+ 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ 输出：7 -> 0 -> 8
+ 原因：342 + 465 = 807
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/add-two-numbers
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 
 /**
  * Definition for singly-linked list.
@@ -26,29 +37,24 @@ class ListNode {
     }
 }
 
-//todo:边界溢出
-
-// [9]
-//[1,9,9,9,9,9,9,9,9,9]
-
 class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
         //基本测试
-        ListNode l3 = solution.addTwoNumbers(solution.long2ListNode(342), solution.long2ListNode(465));
-        sout(l3);
+        ListNode res = solution.addTwoNumbers(solution.long2ListNode(342), solution.long2ListNode(465));
+        sout(res);
 
         //特殊:单链表
-        l3 = solution.addTwoNumbers(solution.long2ListNode(619), solution.long2ListNode(0));
-        sout(l3);
+        res = solution.addTwoNumbers(solution.long2ListNode(619), solution.long2ListNode(0));
+        sout(res);
         //特殊:边界进位
-        l3 = solution.addTwoNumbers(solution.long2ListNode(5), solution.long2ListNode(5));
-        sout(l3);
+        res = solution.addTwoNumbers(solution.long2ListNode(5), solution.long2ListNode(5));
+        sout(res);
 
-        l3 = solution.addTwoNumbers(solution.long2ListNode(1), solution.long2ListNode(99));
-        sout(l3);
+        res = solution.addTwoNumbers(solution.long2ListNode(1), solution.long2ListNode(99));
+        sout(res);
     }
 
     private static void sout(ListNode l3) {
@@ -61,9 +67,9 @@ class Solution {
 
     /**
      * 说明: 使用进位
-     * @author  suwenguang
-     * @date    2019/6/10
-     * @return  n2.ListNode <- 返回类型
+     * @author suwenguang
+     * @date 2019/6/10
+     * @return n2.ListNode <- 返回类型
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         //参数校验
@@ -79,66 +85,66 @@ class Solution {
         //结果链表
         ListNode res = new ListNode(0);
         //临时链表
-        ListNode temp =res;
+        ListNode temp = res;
 
-        while (l1!=null && l2!=null){
+        while (l1 != null && l2 != null) {
             //相加进位
-            int val = l1.val + l2.val+ carrier.val;
-            if (val>=10){
+            int val = l1.val + l2.val + carrier.val;
+            if (val >= 10) {
                 carrier.next = new ListNode(1);
-            }else {
-                carrier.next=new ListNode(0);
+            } else {
+                carrier.next = new ListNode(0);
             }
             //赋值res
             int remain = val % 10;
-            temp.next=new ListNode(remain);
+            temp.next = new ListNode(remain);
 
             //指针移动
-            temp=temp.next;
-            l1=l1.next;
-            l2=l2.next;
-            carrier=carrier.next;
+            temp = temp.next;
+            l1 = l1.next;
+            l2 = l2.next;
+            carrier = carrier.next;
         }
-        while (l1!=null){
+        while (l1 != null) {
             int val = carrier.val + l1.val;
-            if (val>=10){
-                carrier.next=new ListNode(1);
-            }else {
-                carrier.next=new ListNode(0);
+            if (val >= 10) {
+                carrier.next = new ListNode(1);
+            } else {
+                carrier.next = new ListNode(0);
             }
 
             //赋值
             int remain = val % 10;
-            temp.next=new ListNode(remain);
+            temp.next = new ListNode(remain);
 
             //指针移动
-            l1=l1.next;
-            carrier=carrier.next;
-            temp=temp.next;
+            l1 = l1.next;
+            carrier = carrier.next;
+            temp = temp.next;
 
         }
-        while (l2!=null){
+        while (l2 != null) {
             int val = carrier.val + l2.val;
-            if (val>=10){
-                carrier.next=new ListNode(1);
-            }else {
-                carrier.next=new ListNode(0);
+            if (val >= 10) {
+                carrier.next = new ListNode(1);
+            } else {
+                carrier.next = new ListNode(0);
             }
 
             //赋值
             int remain = val % 10;
-            temp.next=new ListNode(remain);
+            temp.next = new ListNode(remain);
 
             //指针移动
-            l2=l2.next;
-            carrier=carrier.next;
-            temp=temp.next;
+            l2 = l2.next;
+            carrier = carrier.next;
+            temp = temp.next;
 
         }
 
         //判定是否还有进位
-        if (carrier.val==1){
-            temp.next=new ListNode(1);
+        if (carrier.val == 1) {
+            temp.next = new ListNode(1);
         }
 
         return res.next;
@@ -146,9 +152,9 @@ class Solution {
 
     /**
      * 说明:暴力破解
-     * @author  suwenguang
-     * @date    2019/6/10
-     * @return  n2.ListNode <- 返回类型
+     * @author suwenguang
+     * @date 2019/6/10
+     * @return n2.ListNode <- 返回类型
      */
     @Deprecated
     public ListNode addTwoNumbersFail(ListNode l1, ListNode l2) {
@@ -174,9 +180,9 @@ class Solution {
 
     /**
      * 说明:  ListNode 转 long
-     * @author  suwenguang
-     * @date    2019/6/10
-     * @return  long <- 返回类型
+     * @author suwenguang
+     * @date 2019/6/10
+     * @return long <- 返回类型
      */
     private long listNode2Long(ListNode l2) {
         int i = 1;
