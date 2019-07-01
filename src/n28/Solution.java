6a8b8b6f27bcc6e316358b7fc1c513606778b47d
suveng <suveng@163.com>
2019-07-01 22:28:56 +0800

    n28-通过

---
+++ src/n28/Solution.java
@@ -0,0 +1,47 @@
+package n28;
+
+/**
+ * description:
+ * @author suwenguang@52tt.com
+ * @date 2019/7/1
+ * @version 1.0.0
+ **/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 实现 strStr() 函数。
+     *
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+     *
+     * 示例 1:
+     *
+     * 输入: haystack = "hello", needle = "ll"
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: haystack = "aaaaa", needle = "bba"
+     * 输出: -1
+     * 说明:
+     *
+     * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+     *
+     * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/implement-strstr
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int strStr(String haystack, String needle) {
+        if (needle==null){
+            return 0;
+        }
+
+        if (haystack==null){
+            return -1;
+        }
+        return haystack.indexOf(needle);
+    }
+}
